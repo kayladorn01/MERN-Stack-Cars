8ebec1bef09916e0014c0cf9982c0af8365c4646
kayladorn01 <111971639+kayladorn01@users.noreply.github.com>
2023-02-09 10:58:14 +0200

    updated files

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CarsDisplay from "./components/Cars";
+
+function App() {
+  return (
+    <div className="App">
+      <CarsDisplay />
+    </div>
+  );
+}
+
+export default App;
